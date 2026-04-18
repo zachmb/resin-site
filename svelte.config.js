2b81a94868906db223dd625052a2491c793b062f
zachmb <zachmb2008@gmail.com>
2026-04-17 19:29:13 -0500

    new landing

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		}),
+		adapter: adapter(),
 		alias: {
 			'@resin/contracts': './src/lib/contracts/index.ts',
 			'@resin/core': './src/lib/core/index.ts'

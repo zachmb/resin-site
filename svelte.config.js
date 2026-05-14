a89c981941d2177b900c10390542ed3b1bbbf3d3
zachmb <zachmb2008@gmail.com>
2026-05-14 15:21:58 -0500

    Switch to adapter-vercel for Vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// Allows local builds even when the dev machine is on an unsupported Node version.
+			// Vercel Serverless Functions support nodejs18.x/nodejs20.x/etc.
+			runtime: 'nodejs20.x'
+		}),
 		alias: {
 			'@resin/contracts': './src/lib/contracts/index.ts',
 			'@resin/core': './src/lib/core/index.ts'
